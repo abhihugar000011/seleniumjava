dcf9e604f223d4c6b1ba6f1d01b4bf08567bfcc9
AbhiHugar11 <abhishek.hugar1234@gmail.com>
2025-08-13 16:53:43 +0530

    Moving changed file @testng Anootation coder

--- src/test/java/basic_Example/AutomationDemoSite.java
+++ src/test/java/basic_Example/AutomationDemoSite.java
@@ -6,25 +6,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.*;
 
-import java.time.Duration;
+public class AutomationDemoSiteTest {
 
-public class AutomationDemoSite {
-    public static void main(String[] as) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demo.automationtesting.in/Alerts.html");
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+    }
 
-        WebElement alertbutton = driver.findElement(By.xpath("//button[contains(text(),'click the button to display an')]"));
-        alertbutton.click();
+    @Test
+    public void testAlertButton() throws InterruptedException {
+        driver.get("https://demo.automationtesting.in/Alerts.html");
+
+        WebElement alertButton = driver.findElement(By.xpath("//button[contains(text(),'click the button to display an')]"));
+        alertButton.click();
+
         Alert alert = driver.switchTo().alert();
         Thread.sleep(2000);
         alert.accept();
-        Thread.sleep(2000);
-        String title = driver.getTitle();
-        Assert.assertTrue(alertbutton.isDisplayed());
 
-        driver.quit();
+        Thread.sleep(2000);
+        Assert.assertTrue(alertButton.isDisplayed(), "Alert button should be visible");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
