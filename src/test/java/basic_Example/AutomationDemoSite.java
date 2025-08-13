b10895a53134a52b3d2a2b78aaf27ad1a37120b2
AbhiHugar11 <abhishek.hugar1234@gmail.com>
2025-08-13 16:56:05 +0530

    Moving changed file @testng Anootation coder

--- src/test/java/basic_Example/AutomationDemoSite.java
+++ src/test/java/basic_Example/AutomationDemoSite.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class AutomationDemoSiteTest {
+public class AutomationDemoSite {
 
     WebDriver driver;
 
