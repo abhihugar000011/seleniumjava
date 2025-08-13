faa5b95029693296f75d1838ae54df9c08e26d2a
AbhiHugar11 <abhishek.hugar1234@gmail.com>
2025-08-13 12:40:13 +0530

    This is factorial code

---
+++ Example1.java
@@ -0,0 +1,11 @@
+package basic_Example;
+
+public class Example1 {
+    public static void main(String[] args) {
+     int n = 5,r=1;
+        for (int i = 1; i <= n; i++) {
+            r = i*r;
+        }
+        System.out.println(r);
+    }
+}
